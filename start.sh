848589e2b824f49d0f6b322e301a70f769f93dfd
BlindBlocArt-Jerry <42042199+BlindBlocArt-Jerry@users.noreply.github.com>
2018-08-28 14:21:34 -0700

    fix typo

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 NODEOS=../telos/build/programs/nodeos/nodeos
 DATADIR=`pwd`
 $DATADIR/stop.sh
-$NODEOS --data-dir $DATADIR --config-dir $DATADIR "$@" > 2>&1 $DATADIR/tlos.log &  echo $! > $DATADIR/nodeos.pid
+$NODEOS --data-dir $DATADIR --config-dir $DATADIR "$@"  &> $DATADIR/tlos.log &  echo $! > $DATADIR/nodeos.pid
