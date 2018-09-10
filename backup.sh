d047877d04889e246a95d76b56e80a2fe14a3a19
BlindBlocArt-Jerry <42042199+BlindBlocArt-Jerry@users.noreply.github.com>
2018-09-10 14:56:13 -0700

    Keep two copies of tar file

--- backup.sh
+++ backup.sh
@@ -5,7 +5,8 @@
 cp -r blocks blockstemp
 cp -r state statetemp
 ./start.sh
-rm backup.tar.gz
+rm backup.tar.gz.1
+mv backup.tar.gz backup.tar.gz.1
 tar czvf backup.tar.gz blockstemp statetemp
 rm -rf blockstemp
 rm -rf statetemp
